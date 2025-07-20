6f6526eae6bed8af09512228e50c0ca8d61b92af
Empya1 <161871238+Empya1@users.noreply.github.com>
2025-07-20 16:56:30 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,37 +1,62 @@
 
+    
+window.addEventListener("load", ()=>{
+    
+    setTimeout(()=>{
+        let hiddenItems = document.querySelectorAll(".hide-before-load")
+        hiddenItems.forEach((item)=>{
+            item.style.display = "block";
+        })
+        
+        }, 5500)
+        
+    setTimeout(()=>{
+        var splash = document.querySelector(".splash");
+        splash.style.display = "None"
+    },5000);
+})
+
 document.addEventListener("DOMContentLoaded", ()=>{
+
+
     var sidenav = document.querySelector(".sidenav");
     var sidenavOptions = {
         draggable:true
     };
     var sideNav = M.Sidenav.init(sidenav, sidenavOptions);
     
-    var floatingbtns = document.querySelectorAll(".fixed-action-btn")
-    var floatingbtnOptions = {}
-    M.FloatingActionButton.init(floatingbtns, floatingbtnOptions);
+    var floatingbuttons = document.querySelectorAll(".fixed-action-btn");
+    var floatingbtnOptions = {
+        
+    };
+    var floatingbuttons = M.FloatingActionButton.init(floatingbuttons, floatingbtnOptions);
     
-    var sliders = document.querySelectorAll(".slider")
-    M.Slider.init(sliders, {interval:6000, indicators:false, duration:2000});
+    var sliders = document.querySelectorAll(".slider");
+    var sliderOptions = {
+        interval:6000, 
+        indicators:false, 
+        duration:2000
+    }
+    M.Slider.init(sliders, sliderOptions);
     
     
-    let animelems = document.querySelectorAll(".slide-in-anim");
     
-    document.addEventListener("scroll", (a)=>{
-        animelems.forEach((elem)=>{
-            let rect = elem.getBoundingClientRect()
-            if (window.innerHeight - rect.top > elem.clientHeight*0.25){
-                elem.style.transform = "translateY(0)"
-                elem.style.opacity = 1
+    
+    let animatedElements = document.querySelectorAll(".slide-in-anim");
+    
+    document.addEventListener("scroll", (event)=>{
+        animatedElements.forEach((element)=>{
+            let rect = element.getBoundingClientRect()
+            if (window.innerHeight - rect.top > element.clientHeight*0.25){
+                element.style.transform = "translateY(0)"
+                element.style.opacity = 1
             }
-            
         })
     })
-   
-   
 });
 
-function closeNav(){
-        var sideNav = M.Sidenav.getInstance(document.querySelector(".sidenav"))
-        sideNav.close()
-    }
+function closeNav(){ 
+    var sideNav = M.Sidenav.getInstance(document.querySelector(".sidenav"));
+    sideNav.close();
+}
 
